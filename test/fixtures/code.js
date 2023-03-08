175627d41a9752d78ce0436e9096dbb58b9615bc
CarlosL <carlos.ledesma1983@gmail.com>
2023-03-09 00:41:51 +0100

    lint chore and update fixtures

--- test/fixtures/code.js
+++ test/fixtures/code.js
@@ -1,18 +1,18 @@
 
 function lospepeh() {
-
     const pepe = 1;
     const pepa = 2;
-    const pepo = [1,2,3,4,5,6];
+    const pepo = [1, 2, 3, 4, 5, 6];
 
     const pepi = pepo.map(piper => {
-        if(piper === pepe) {
+        if (piper === pepe) {
             console.log('pepe');
-        } else if(piper === pepa) {
+        } else if (piper === pepa) {
             console.log('pepa');
         } else {
             console.log('pepo');
         }
-        return piper+5;
-    })
+
+        return piper + 5;
+    });
 }
--- test/urlManager.test.js
+++ test/urlManager.test.js
@@ -2,28 +2,25 @@ const path = require('path');
 const axios = require('axios');
 const fs = require('fs-extra');
 
-const getFileFromUrl = require('../src/application/urlManager');
+const {getFileFromUrl} = require('../src/application/urlManager');
 
 jest.mock('axios');
-//Write a jest default template
+
 describe('Url Manager', () => {
+    beforeEach(() => {
+    });
+    it('should return a valid output from the given url', async () => {
+    // Given
+        const url = 'https://raw.githubusercontent.com/munchkin09/node-csgo/master/handlers/player.js';
+        const expectedResult = await fs.readFile(path.resolve('test/fixtures/expectedOutputFromUrl.txt'), 'utf8');
+        const response = {status: 200, data: await fs.readFile(path.resolve('test/fixtures/code.js'))};
 
-  beforeEach(() => {
-  });
-  it('should return a valid output from the given url', async () => {
+        axios.get.mockResolvedValue(response);
 
-    //Given
-    const url = "https://raw.githubusercontent.com/munchkin09/node-csgo/master/handlers/player.js";
-    const expectedResult = await fs.readFile(path.resolve('test/fixtures/expectedOutputFromUrl.txt'), 'utf8');
-    const response = {status: 200, data: await fs.readFile(path.resolve('test/fixtures/code.js')) }
+        // When
+        const result = await getFileFromUrl(url);
 
-    axios.get.mockResolvedValue(response);
-
-    //When
-    const result = await getFileFromUrl(url)
-
-    //Then
-    expect(result.toString()).toBe(expectedResult);
-
-  });
+        // Then
+        expect(result.toString()).toBe(expectedResult);
+    });
 });
