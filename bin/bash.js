415d02b657a0862f0175eb8f1a27872d912f6d69
CarlosL <carlos.ledesma1983@gmail.com>
2023-03-11 15:15:46 +0100

    small refactor 2

--- bin/bash.js
+++ bin/bash.js
@@ -3,4 +3,6 @@ const basicExecutor = require('../src/index');
 const url = process.argv[2];
 const configurationPath = process.argv[3];
 
-await basicExecutor(url, configurationPath);
+(async function () {
+    await basicExecutor(url, configurationPath);
+})();
