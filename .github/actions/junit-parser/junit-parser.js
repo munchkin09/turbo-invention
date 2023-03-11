0f041232944adbcca6c764cc9016fc7f3d385ed3
CarlosL <carlos.ledesma1983@gmail.com>
2023-03-11 16:34:33 +0100

    update CI 5

--- .github/actions/junit-parser/junit-parser.js
+++ .github/actions/junit-parser/junit-parser.js
@@ -1,22 +1,26 @@
-const core = require("@actions/core");
+const core = require('@actions/core');
 const github = require("@actions/github");
 
 const { XMLParser, XMLBuilder } = require("fast-xml-parser");
 
-try {
-    const xmlReport = core.getInput("junit-parser");
+async function run() {
+    try {
+        const xmlReport = core.getInput("junit-parser");
+    
+        const parser = new XMLParser();
+        let jObj = parser.parse(xmlReport);
+        const builder = new XMLBuilder();
+        const xmlContent = builder.build(jObj);
+    
+        core.setOutput("html-report", xmlContent);
+    
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2);
+        console.log(`The event payload: ${payload}`);
+    
+    } catch (error) {
+        core.setFailed(error.message);
+    }}
 
-    const parser = new XMLParser();
-    let jObj = parser.parse(xmlReport);
-    const builder = new XMLBuilder();
-    const xmlContent = builder.build(jObj);
+run();
 
-    core.setOutput("html-report", xmlContent);
-
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
-
-} catch (error) {
-    core.setFailed(error.message);
-}
