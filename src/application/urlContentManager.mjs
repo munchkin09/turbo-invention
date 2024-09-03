e0c1e74fb719ba8f8eb7a390725cac97d038c36b
CarlosL <carlos.ledesma1983@gmail.com>
2024-09-03 10:49:18 -0500

    Delete noise lines

--- src/application/urlContentManager.mjs
+++ src/application/urlContentManager.mjs
@@ -3,22 +3,18 @@
 // \s = space
 
 export async function getFileFromUrl(url) {
-    // Retrieve file from given url using axios
+    // Retrieve file from given url using fetch
     const response = await fetch(url);
-    // TODO Make test for error handling
 
     if (response.status !== 200) {
         throw new Error('The file could not be retrieved');
     }
 
-    // Convert response to plain text
     let fileContent;
     try {
-        // TODO Make test for error handling
         fileContent = response.data;
     } catch (error) {
-        console.log(error);
-        // throw new Error('The file is not a text file', error);
+        throw new Error('The file is not a text file', error);
     }
 
     return fileContent;
