b77f08eeb444383d2602bcb0393b341702f94096
CarlosL <carlos.ledesma1983@gmail.com>
2024-09-03 10:28:56 -0500

    Added mock for fetch

--- test/main.e2e.test.mjs
+++ test/main.e2e.test.mjs
@@ -18,28 +18,26 @@ describe('Test execution interfaces', () => {
     const executionPath = path.resolve('bin','bash.js');
     const url = 'https://raw.githubusercontent.com/munchkin09/node-csgo/master/handlers/player.js';
     const configurationPath = path.resolve('test','fixtures', 'configuration','config.json');
-    const response = {status: 200, data: await fs.readFile(path.resolve('test/fixtures/code.js'))};
-    jest.unstable_mockModule('axios',() => {
-      get: jest.fn().mockResolvedValue(response);
-    });
+    const response = {status: 200, data: await fs.readFile(path.resolve('test/fixtures/code.js'), 'utf8')};
+    jest.spyOn(global, 'fetch').mockResolvedValue(response);
     // When
     await execute(url, configurationPath);
 
     // Then
     expect(log.mock.calls[0][0]).toBe('CleanCrappers is running...');
-    expect(log.mock.calls[1][0]).toBe('########I#I#I######');
-    expect(log.mock.calls[2][0]).toBe('########I#I#I######');
-    expect(log.mock.calls[3][0]).toBe('########I#I#I######');
-    expect(log.mock.calls[4][0]).toBe('########I#I#I######');
-    expect(log.mock.calls[5][0]).toBe('#######IIIIIII#I###');
-    expect(log.mock.calls[6][0]).toBe('#######IIIIIII#I###');
-    expect(log.mock.calls[7][0]).toBe('#######IIIIIII#I###');
-    expect(log.mock.calls[8][0]).toBe('#######IIIIIII#I###');
-    expect(log.mock.calls[9][0]).toBe('##III#IIIIIIII#II##');
-    expect(log.mock.calls[10][0]).toBe('##III#IIIIIIII#II##');
-    expect(log.mock.calls[11][0]).toBe('##III#IIIIIIII#II##');
-    expect(log.mock.calls[12][0]).toBe('##III#IIIIIIII#II##');
-    expect(log.mock.calls[13][0]).toBe('▁▁¡¡¡▁¡¡¡¡¡¡¡¡▁¡¡▁▁');
+    expect(log.mock.calls[1][0]).toBe('########I#I#I############');
+    expect(log.mock.calls[2][0]).toBe('########I#I#I############');
+    expect(log.mock.calls[3][0]).toBe('########I#I#I############');
+    expect(log.mock.calls[4][0]).toBe('########I#I#I############');
+    expect(log.mock.calls[5][0]).toBe('#######IIIIIII#I#########');
+    expect(log.mock.calls[6][0]).toBe('#######IIIIIII#I#########');
+    expect(log.mock.calls[7][0]).toBe('#######IIIIIII#I#########');
+    expect(log.mock.calls[8][0]).toBe('#######IIIIIII#I#########');
+    expect(log.mock.calls[9][0]).toBe('##III#IIIIIIII#II########');
+    expect(log.mock.calls[10][0]).toBe('##III#IIIIIIII#II########');
+    expect(log.mock.calls[11][0]).toBe('##III#IIIIIIII#II########');
+    expect(log.mock.calls[12][0]).toBe('##III#IIIIIIII#II########');
+    expect(log.mock.calls[13][0]).toBe('▁▁¡¡¡▁¡¡¡¡¡¡¡¡▁¡¡▁▁▁▁▁▁▁▁');
   });
 
 });
